c81c11c57d18f9a819f4efeceb7dc3b42f543dec
phelipe <phelipegeraldo@hotmail.com>
2025-07-04 15:23:08 -0300

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,11 @@ import { AuthProvider } from "@/context/AuthContext"; // Ajuste o caminho se nec
 import Header from "@/components/Header";       // Ajuste o caminho se necessário
 import WhatsAppButton from '@/components/WhatsAppButton'; // <-- Importar o novo componente
 
-const imageUrl = 'https://i.imgur.com/pKFUgKT.png';
+// const imageUrl = 'https://i.imgur.com/pKFUgKT.png';
+// const imageUrl = 'https://www.sorteioativo.com.br/logo.jpg';
+const imageUrl = 'https://205d-191-183-36-8.ngrok-free.app/logosorteioativo7.jpeg';
+// const imageUrl = 'https://trocafone.vtexassets.com/arquivos/ids/396113-800-auto?v=638732593373830000&width=800&height=auto&aspect=true';
+const icon = '/favicon.ico'
 const title = 'Site de Sorteios';
 const description = 'Participe dos nossos sorteios e ganhe prêmios incríveis!';
 const siteName = 'Site de Sorteios';
@@ -14,7 +18,7 @@ export const metadata: Metadata = {
   title: title,
   description: description,
   icons: { // Recomendo esta estrutura para `icons`
-    icon: imageUrl, // URL para o favicon principal
+    icon: icon, // URL para o favicon principal
     // Você pode adicionar outros tamanhos ou tipos aqui, se tiver:
     // apple: '/apple-icon.png',
     // shortcut: '/shortcut-icon.png',
@@ -22,18 +26,19 @@ export const metadata: Metadata = {
   openGraph: {
     title: title,
     description: description,
-    images: [{ url: imageUrl, width: 1200, height: 630 }], // Correção principal aqui
+    images: [{ url: imageUrl, width: 800, height: 'auto',alt: title }], // Correção principal aqui
     // Removidas as linhas 'images width:1200,' e 'image height:630,' que estavam duplicadas e com sintaxe incorreta.
     // As dimensões (width e height) devem estar DENTRO do objeto da imagem no array 'images'.
     siteName: siteName,
     locale: 'pt_BR',
     type: 'website',
+    url: 'https://www.sorteioativo.com.br',
   },
   twitter: {
     card: 'summary_large_image',
     title: title, // Melhor explicitar, embora `title` e `description` funcionem se forem variáveis no escopo
     description: description, // Melhor explicitar
-    images: [{ url: imageUrl, width: 1200, height: 675 }], // Recomendo adicionar width/height também para o Twitter para otimização
+    images: [{ url: imageUrl, width: 1200, height: 675, alt: title }], // Recomendo adicionar width/height também para o Twitter para otimização
   },
 };
 
